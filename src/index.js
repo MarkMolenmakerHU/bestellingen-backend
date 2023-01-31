e1af387ce7164932af6ca1a866fb7c3851b06817
inazumark <inazumarkmail@gmail.com>
2023-01-31 11:19:18 +0100

    added more cors to allow netlify app

--- src/index.js
+++ src/index.js
@@ -12,6 +12,12 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
+});
+
 app.use(express.json());
 
 app.use('/api', routes);
